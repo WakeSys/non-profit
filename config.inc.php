fad5d2d368d350ec3757282bb3d43a76705e2cb3
chrizzellu <chrizzellu@users.noreply.github.com>
2021-03-26 17:17:07 +0100

    loading autoload

--- config.inc.php
+++ config.inc.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once __DIR__.'/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__.'/..');
+$dotenv->load();
+
 $mysqldb = getenv("DB_DATABASE");		// DB name 
 $mysqluser = getenv("DB_USERNAME");		// Benutzername fur den MySQL-Zugang 
 $mysqlpasswd = getenv("DB_PASSWORD");	// Passwort 
 $mysqlhost = getenv("DB_HOST");			// Name des Rechners, auf dem MySQL laeuft 
 
 
-echo '$mysqlhost: ' . $mysqlhost . '<br>';
+// echo '$mysqlhost: ' . $mysqlhost . '<br>';
