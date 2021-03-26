15838d42ee3480f98c7ffaa808916f97ba72b00e
chrizzellu <chrizzellu@users.noreply.github.com>
2021-03-26 17:08:31 +0100

    .

--- config.inc.php
+++ config.inc.php
@@ -4,3 +4,6 @@ $mysqldb = getenv("DB_DATABASE");		// DB name
 $mysqluser = getenv("DB_USERNAME");		// Benutzername fur den MySQL-Zugang 
 $mysqlpasswd = getenv("DB_PASSWORD");	// Passwort 
 $mysqlhost = getenv("DB_HOST");			// Name des Rechners, auf dem MySQL laeuft 
+
+
+echo '$mysqlhost: ' . $mysqlhost . '<br>';
