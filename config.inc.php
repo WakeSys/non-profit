9efd4491c3034741e29001082f7d38c573fc991e
chrizzellu <chrizzellu@users.noreply.github.com>
2021-03-26 17:18:27 +0100

    .

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__.'/..');
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
 
 $mysqldb = getenv("DB_DATABASE");		// DB name 
