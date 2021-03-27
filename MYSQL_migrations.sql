41aba323959268662af08aa455cb1b172ded61f6
chrizzellu <chrizzellu@users.noreply.github.com>
2021-03-27 05:50:49 +0100

    mysql migrations

---
+++ MYSQL_migrations.sql
@@ -0,0 +1,20 @@
+#Disable ONLY_FULL_GROUP_BY
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
+#Fix Umlaute
+SET SQL_SAFE_UPDATES=0;
+UPDATE `db_name`.`members` SET `first_name` = REPLACE(`first_name`, 'Ã¼', 'ü');
+UPDATE `db_name`.`members` SET `last_name` = REPLACE(`last_name`, 'Ã¼', 'ü');
+UPDATE `db_name`.`members` SET `town` = REPLACE(`town`, 'Ã¼', 'ü');
+UPDATE `db_name`.`members` SET `address` = REPLACE(`address`, 'Ã¼', 'ü');
+
+UPDATE `db_name`.`members` SET `first_name` = REPLACE(`first_name`, 'Ã¤', 'ä');
+UPDATE `db_name`.`members` SET `last_name` = REPLACE(`last_name`, 'Ã¤', 'ä');
+UPDATE `db_name`.`members` SET `town` = REPLACE(`town`, 'Ã¤', 'ä');
+UPDATE `db_name`.`members` SET `address` = REPLACE(`address`, 'Ã¤', 'ä');
+
+UPDATE `db_name`.`members` SET `first_name` = REPLACE(`first_name`, 'Ã¶', 'ö');
+UPDATE `db_name`.`members` SET `last_name` = REPLACE(`last_name`, 'Ã¶', 'ö');
+UPDATE `db_name`.`members` SET `town` = REPLACE(`town`, 'Ã¶', 'ö');
+UPDATE `db_name`.`members` SET `address` = REPLACE(`address`, 'Ã¶', 'ö');
+SET SQL_SAFE_UPDATES=1;
