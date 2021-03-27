6530b7dfb5bba61bbffdf651512fb7fb5ac948e9
chrizzellu <chrizzellu@users.noreply.github.com>
2021-03-27 18:26:44 +0100

    removing sendgrid debug

--- functions/common.inc.php
+++ functions/common.inc.php
@@ -30,9 +30,9 @@ function first_mail($memberID) {
 	$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 	try {
 		$response = $sendgrid->send($email);
-		print $response->statusCode() . "\n";
-		print_r($response->headers());
-		print $response->body() . "\n";
+		// print $response->statusCode() . "\n";
+		// print_r($response->headers());
+		// print $response->body() . "\n";
 	} catch (Exception $e) {
 		echo 'Caught exception: '. $e->getMessage() ."\n";
 	}
@@ -130,9 +130,9 @@ function setPaid($memberID,$value,$paymentID,$loginID,$status,$type="member",$no
 						$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 						try {
 							$response = $sendgrid->send($email);
-							print $response->statusCode() . "\n";
-							print_r($response->headers());
-							print $response->body() . "\n";
+							// print $response->statusCode() . "\n";
+							// print_r($response->headers());
+							// print $response->body() . "\n";
 						} catch (Exception $e) {
 							echo 'Caught exception: '. $e->getMessage() ."\n";
 						}
@@ -183,9 +183,9 @@ function setPaid($memberID,$value,$paymentID,$loginID,$status,$type="member",$no
 							$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 							try {
 								$response = $sendgrid->send($email);
-								print $response->statusCode() . "\n";
-								print_r($response->headers());
-								print $response->body() . "\n";
+								// print $response->statusCode() . "\n";
+								// print_r($response->headers());
+								// print $response->body() . "\n";
 							} catch (Exception $e) {
 								echo 'Caught exception: '. $e->getMessage() ."\n";
 							}
